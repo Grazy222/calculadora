219eb7f3b91b75604b3cb41efd9cc9db63f00950
Grazy Pérola <graciele222@gmail.com>
2023-10-22 21:05:02 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# calculadora
-Created with CodeSandbox
+# Simple-Calculator
+This is a Simple Calculator with Addition, Subtraction, Multiplication, Division, Square Root, Square and Cube Operations Built Purely with React JS 
---
+++ src/App.js
@@ -0,0 +1,113 @@
+import React from "react";
+import "./styles.css";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import EqualButton from "./components/EqualButton";
+
+export default function App({ state, dispatch }) {
+  return (
+    <div className="App">
+      <h1>Calculadora com React</h1>
+      <div className="calc-wrapper">
+        <Input input={state}></Input>
+        <div className="row">
+          <Button
+            onClick={() => dispatch({ type: "addtoInputNum", param: "7" })}
+          >
+            7
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: "addtoInputNum", param: "8" })}
+          >
+            8
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: "addtoInputNum", param: "9" })}
+          >
+            9
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: "addtoInputOpr", param: "/" })}
+          >
+            /
+          </Button>
+          <Button onClick={() => dispatch({ type: "handleRoot" })}>√</Button>
+        </div>
+        <div className="row">
+          <Button
+            onClick={() => dispatch({ type: "addtoInputNum", param: "4" })}
+          >
+            4
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: "addtoInputNum", param: "5" })}
+          >
+            5
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: "addtoInputNum", param: "6" })}
+          >
+            6
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: "addtoInputOpr", param: "*" })}
+          >
+            X
+          </Button>
+          <Button onClick={() => dispatch({ type: "handleCube" })}>x³</Button>
+        </div>
+        <div className="row">
+          <Button
+            onClick={() => dispatch({ type: "addtoInputNum", param: "1" })}
+          >
+            1
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: "addtoInputNum", param: "2" })}
+          >
+            2
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: "addtoInputNum", param: "3" })}
+          >
+            3
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: "addtoInputOpr", param: "+" })}
+          >
+            +
+          </Button>
+          <Button onClick={() => dispatch({ type: "handleSquare" })}>x²</Button>
+        </div>
+        <div className="row">
+          <Button
+            onClick={() => dispatch({ type: "addtoInputNum", param: "." })}
+          >
+            .
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: "addtoInputNum", param: "0" })}
+          >
+            0
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: "addtoInputNum", param: "00" })}
+          >
+            00
+          </Button>
+          <Button
+            onClick={() => dispatch({ type: "addtoInputNum", param: "-" })}
+          >
+            -
+          </Button>
+          <Button onClick={() => dispatch({ type: "handleClear" })}>C</Button>
+        </div>
+        <div className="row">
+          <EqualButton onClick={() => dispatch({ type: "handleEqual" })}>
+            =
+          </EqualButton>
+        </div>
+      </div>
+    </div>
+  );
+}
